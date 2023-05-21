38f42bde627744b276531cf6a0b652f81245ce61
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-05-21 18:26:44 +0100

    Some Changes

--- app/src/main/java/com/example/animatedgradient/MainActivity.kt
+++ app/src/main/java/com/example/animatedgradient/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.animatedgradient
 
-import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,20 +7,23 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -33,11 +35,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AnimatedGradientTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    GradientCircle(
-                        colorPalette
-                    )
+                    AnimatedShadowCard()
                 }
             }
         }
@@ -45,63 +44,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
-@SuppressLint("RememberReturnType")
 @Composable
-fun GradientCircle(
-    colorPalette : List<Color>
-) {
-    val infiniteTransition = rememberInfiniteTransition()
-    val color1 by infiniteTransition.animateColor(
-        initialValue = colorPalette[2],
-        targetValue = colorPalette[1],
-        animationSpec = infiniteRepeatable(
-            animation = tween(3000, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-    val color2 by infiniteTransition.animateColor(
-        initialValue = colorPalette[0],
-        targetValue = colorPalette[1],
-        animationSpec = infiniteRepeatable(
-            animation = tween(4000, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-    val color3 by infiniteTransition.animateColor(
-        initialValue = colorPalette[2],
-        targetValue = colorPalette[1],
-        animationSpec = infiniteRepeatable(
-            animation = tween(2500, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-
-    BoxWithConstraints(
+fun AnimatedShadowCard(){
+    Box(
         modifier = Modifier
             .fillMaxSize()
-    ) {
-        Card(
-            colors = CardDefaults.cardColors(
-                color2
+            .background(Color.Black)
 
-            ),
-            shape = CircleShape,
+    ){
+        val color1 = Color(0xfffa300c)
+        val color2 = Color(0xff48d7ff)
+
+        Box  (
+
             modifier = Modifier
                 .align(Alignment.Center)
-                .size(maxWidth/2)
-
-                .drawColoredShadow(
-                    shape = RoundedCornerShape(),
-                    color = color1,
-                    offsetY = 40.dp,
+                .drawAnimatedColoredShadow(
+                    color1,
+                    color2,
+                    offsetY = 0.dp,
                     offsetX = 0.dp,
-                    alpha = 1f
+                    shadowRadius = 60.dp,
+                    borderRadius = 20.dp,
+                    alpha = 0.4f
                 )
-            ,
-        ) {
+        ){
+            Image(
+                painter = painterResource(R.drawable.poke_card),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
 
         }
+
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun Modifier.drawAnimatedColoredShadow(
+    initialColor : Color,
+    targetColor : Color,
+    alpha: Float = 0.2f,
+    borderRadius: Dp = 0.dp,
+    shadowRadius: Dp = 20.dp,
+    offsetY: Dp = 0.dp,
+    offsetX: Dp = 0.dp
+): Modifier = composed {
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val color by infiniteTransition.animateColor(
+        initialValue = initialColor,
+        targetValue = targetColor,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    drawColoredShadow(
+        color,
+        alpha,
+        borderRadius, shadowRadius, offsetY, offsetX
+    )
+
 }
 
 val colorPalette = listOf<Color>(
@@ -123,7 +127,6 @@ fun DefaultPreview() {
 
 @RequiresApi(Build.VERSION_CODES.O)
 fun Modifier.drawColoredShadow(
-    shape: ShapeDefaults,
     color: Color,
     alpha: Float = 0.2f,
     borderRadius: Dp = 0.dp,
@@ -131,8 +134,12 @@ fun Modifier.drawColoredShadow(
     offsetY: Dp = 0.dp,
     offsetX: Dp = 0.dp
 ) = this.drawBehind {
-    val transparentColor = android.graphics.Color.toArgb(color.copy(alpha = 0.0f).value.toLong())
-    val shadowColor = android.graphics.Color.toArgb(color.copy(alpha = alpha).value.toLong())
+    val transparentColor = android.graphics.Color.toArgb(
+        color.copy(alpha = 0.0f).value.toLong()
+    )
+    val shadowColor = android.graphics.Color.toArgb(
+        color.copy(alpha = alpha).value.toLong()
+    )
     this.drawIntoCanvas {
         val paint = Paint()
         val frameworkPaint = paint.asFrameworkPaint()
@@ -143,9 +150,7 @@ fun Modifier.drawColoredShadow(
             offsetY.toPx(),
             shadowColor
         )
-        when(shape){
-            is
-        }
+
         it.drawRoundRect(
             0f,
             0f,
